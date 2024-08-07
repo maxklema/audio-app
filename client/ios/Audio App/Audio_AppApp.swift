2b0bd1cdf8f7828175ceff096e8717d7447707d5
maxklema <maxklema@gmail.com>
2024-08-07 17:20:53 -0400

    IOS swift code initialization -> Opus package installed

---
+++ client/ios/Audio App/Audio_AppApp.swift
@@ -0,0 +1,23 @@
+//
+//  Audio_AppApp.swift
+//  Audio App
+//
+//  Created by Maxwell Klema on 8/7/24.
+//
+
+import SwiftUI
+import Opus
+
+@main
+struct Audio_AppApp: App {
+    
+    init() {
+        print("Audio_AppApp initialized")
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
